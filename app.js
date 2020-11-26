7e0de5d41603170d736f5044228160ddc53d3794
Jean-Baptiste Lavisse <jean-baptiste@jblavisse.fr>
2020-11-26 17:26:44 +0100

    Create Link in Nav

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 'use strict';
+
+/*
+Reproduire en JS seulement ! 
+
+<nav>
+	<a href="/">Accueil</a>
+	<a href="/services">Mes services</a>
+	<a href="/contact">Contact</a>
+</nav>
+
+*/
+
+/* 
+Etape 1 -> Créer la nav et la placer dans le DOM HTML 
+NB: Quand on crée un élément avec document.createElement, il est pour l'instant dissocié du DOM
+Si on veut le lier au DOM, il faudra l'insérer grâce à une méthode via un élément de référence
+Element de référence = Par rapport à quel autre élément de la page, je veux le placer 
+*/
+
+const nav = document.createElement("nav");
+console.log(nav);
+
+// const body = document.querySelector("body");
+// Raccourci ->
+const body = document.body;
+
+body.prepend(nav);
+
+function createLinkInNav(url, text) {
+    const link = document.createElement("a");
+    link.href = url;
+    link.textContent = text;
+    nav.append(link);
+}
+
+createLinkInNav("/","Accueil");
+createLinkInNav("/services", "Mes services");
+createLinkInNav("/contact", "Contact");
